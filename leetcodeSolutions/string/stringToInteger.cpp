c307d53c812cc597548670395288a884ae3fbbb3
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-01-28 08:08:09 +0100

    string to integer conversion

---
+++ leetcodeSolutions/string/stringToInteger.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+      if(s.empty())
+        return 0;
+      int len = s.length(),i=0,sign = 1;
+      
+      while(i<len && s[i] == ' ')
+        i++;
+      if(i== len)
+        return 0;
+      if(s[i] == '-'){
+        sign = 0;
+        i++;
+      }
+      else if(s[i] == '+')
+        i++;
+      long long int out = 0;
+      
+      while(s[i]>='0' && s[i] <= '9'){
+        out = out * 10;
+        if(out <= INT_MIN || out >= INT_MAX)
+          break;
+        out = out + (s[i] - '0');
+        i++;
+      }
+      if(sign == 0)
+        out = -1 * out;
+      if(out <= INT_MIN )
+        return  INT_MIN;
+      if(out >= INT_MAX)
+        return INT_MAX;
+      return (int)out;
+    }
+};
