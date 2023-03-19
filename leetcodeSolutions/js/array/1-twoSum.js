67242525082761d9fd3ac057953738425a6ead63
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-03-19 17:34:37 +0100

    two sum

---
+++ leetcodeSolutions/js/array/1-twoSum.js
@@ -0,0 +1,40 @@
+
+// Nested loop approach
+const twoSum = (nums,target) => {
+  for (let i = 0;i <nums.length; i++ ){
+    // Determine the complement (required number) for the current number
+    const complement = target - nums[i]
+
+    // Loop through the array again
+    for (let x = 0;x < nums.length ; x++){
+      // Check if the current number is the first number's complement
+      if (x !== i && nums[x] === complement){
+        // Return both numbers
+        return [i,x]
+      }
+    }
+  }
+}
+
+
+// Mapping method
+// const twoSum =  (nums, target) => {
+//   // Initialise a map to store the first run of numbers
+//   const mapOfNumbers = new Map();
+
+//   // Loop through the numbers
+//   for (let i = 0; i < nums.length; i++) {
+//     // Determine the complement (required number) for the current number
+//     const complement = target - nums[i];
+
+//     // Check if the complement exists in the Map
+//     if (mapOfNumbers.has(complement)) {
+//         return [mapOfNumbers.get(complement), i];
+//     }
+
+//       // If not, add the current number to the Map
+//     mapOfNumbers.set(nums[i], i);
+//   }
+// };
+
+console.log(twoSum([1,2,2,3],4))
