ee5ff88644aee492ab4a3c9cda44160b8424fe18
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-03-21 14:42:10 +0100

    merging two sorted arrays

---
+++ leetcodeSolutions/js/array/88-mergeSortedArray.js
@@ -0,0 +1,38 @@
+
+const merge = (nums1,m,nums2,n)=>{
+  // initializa pointers at the starting point of each of the arrays
+  let i = 0
+  let j = 0
+
+  // Initialize the array that will contain the merged values
+  const result = []
+
+  // Iterate over the input arrays until one of them is empty
+  while (i < m && j < n){
+    // for eacher pointer position, add value from the smallest of the 2 array
+    if(nums1[i] < nums2[j]){
+      result.push(nums1[i])
+      i++
+    }else{
+      result.push(nums2[j])
+      j++
+    }
+  }
+
+  // add any remaining element from nums1 to the merged array
+  while(i<m){
+    result.push(nums1[i])
+    i++
+  }
+
+  // Add any remaining elements from nums 2 to the merged array
+  while(j<n){
+    result.push(nums2[j])
+    j++
+  }
+
+  // copy the results back into nums 1 or nums2
+  for (let k = 0; k < m + n; k++){
+    nums1[k] = result[k]
+  }
+}
