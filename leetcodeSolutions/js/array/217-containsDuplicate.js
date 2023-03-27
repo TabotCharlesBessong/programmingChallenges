0b41898b4b6ea98e63b5e0954ed27501d46053b0
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-03-27 22:27:28 +0100

    contains duplicates

---
+++ leetcodeSolutions/js/array/217-containsDuplicate.js
@@ -0,0 +1,15 @@
+
+const containsDuplicate = (nums) => {
+  if(nums.length <= 1) return false
+
+  for(let i=0;i<nums.length-1;i++){
+    let j=i+1;
+    while(j<nums.length){
+      if(nums[i]===nums[j]){
+        return true;
+      }
+      j++;
+    }
+  }
+  return false;
+}
