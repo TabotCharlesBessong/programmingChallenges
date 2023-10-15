25b5362b72273cf583a10d5bd784bd7e46319f84
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-15 17:52:26 +0100

    valid sudoku problem

--- leetcodeSolutions/js/array/36-validSudoku.js
+++ leetcodeSolutions/js/array/36-validSudoku.js
@@ -35,6 +35,64 @@
 //   return true
 // }
 
-const isValidSudoku = (board) => {
-  
+function isValidSudoku(board) {
+  const rows = Array(9)
+    .fill(0)
+    .map(() => new Set());
+  const columns = Array(9)
+    .fill(0)
+    .map(() => new Set());
+  const boxes = Array(9)
+    .fill(0)
+    .map(() => new Set());
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const cell = board[i][j];
+      if (cell !== ".") {
+        if (
+          rows[i].has(cell) ||
+          columns[j].has(cell) ||
+          boxes[Math.floor(i / 3) * 3 + Math.floor(j / 3)].has(cell)
+        ) {
+          return false;
+        }
+        rows[i].add(cell);
+        columns[j].add(cell);
+        boxes[Math.floor(i / 3) * 3 + Math.floor(j / 3)].add(cell);
+      }
+    }
+  }
+
+  return true;
 }
+
+const board = [
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+];
+
+const board1 = [
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "8"], // Invalid: Duplicate '8' in the last cell of the last row
+];
+
+const isValid1 = isValidSudoku(board1);
+console.log(isValid1);
+
+const isValid = isValidSudoku(board);
+console.log(isValid);
