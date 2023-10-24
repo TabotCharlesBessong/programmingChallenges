8774aea074110cc5d44400934f7bec9f417759cf
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-24 23:46:21 +0100

    reverse bits

---
+++ leetcodeSolutions/js/divideAndConquer/190-reverseBit.js
@@ -0,0 +1,18 @@
+
+/**
+ * @param {number} n - a 32-bit unsigned integer
+ * @return {number} - a 32-bit unsigned integer representing the reversed bits
+ */
+var reverseBits = function (n) {
+  // var s = "";
+  var count = 0;
+  var index = 31;
+  while (n > 0) {
+    if (n % 2 !== 0) count += Math.pow(2, index);
+    index--;
+    n = Math.floor(n / 2);
+  }
+  return count;
+};
+
+console.log(reverseBits(11111111111111111111111111111101));
